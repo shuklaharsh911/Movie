a0f5b83e16afc4f8b6c1e8d51a3ee6bdfaa9b53f
sharsh911 <54365213+sharsh911@users.noreply.github.com>
2021-01-06 04:24:09 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {useState} from 'react';
-import logo from './logo.svg';
 import ReactDOM from 'react-dom';
 import './App.css';
 import { MovieList } from './MovieList'
